f4f694ee1e7082b7d16b4ef0ecdc654bde969d1c
Erick Galvan <galvanerick27@gmail.com>
2023-12-20 23:56:40 -0600

    memory

--- routes/pdf.py
+++ routes/pdf.py
@@ -1,15 +1,19 @@
 from fastapi import APIRouter, File, UploadFile, HTTPException, Depends
-from routes.utils.pdf_utils import test, extract_text_from_pdf, get_text_chunks
+from routes.utils.pdf_utils import test, extract_text_from_pdf, get_text_chunks, get_embeddings, get_conversation_chain, handle_user_input
 from typing import Annotated
 from functools import lru_cache
 from dotenv import load_dotenv
 import os
 from langchain.llms import openai, llamacpp
+from pydantic import BaseModel
 
 load_dotenv()
 pdfRouter = APIRouter()
 API_KEY = os.getenv( "OPENAI_API_KEY")
 llm_openai = openai.OpenAI(model="text-davinci-003", api_key=API_KEY)
+
+class QuestionData(BaseModel):
+    question: str
 
 @pdfRouter.get("/")
 def read_root():
@@ -20,15 +24,22 @@ async def upload_pdf(file: UploadFile = File(...)):
     try:
         text = extract_text_from_pdf(file.file)
         chunks = get_text_chunks(text)
-        print(len(chunks))
+        vector_store = get_embeddings(chunks)
+        conversation = get_conversation_chain(vector_store)
+        respuesta = conversation({'question': 'de que trata el texto?', 'chat_history': []})
+        print(respuesta)
         
-        # language = detect_language(text)
-        # tokens = process_text(text)
-        # filtered_tokens = remove_stopwords(tokens, language)
-        # lemmatized_tokens = perfom_lemmatization(filtered_tokens)
-        # clean_text = " ".join(lemmatized_tokens)
-        # test(clean_text)
-        return {"filename": file.filename, "text": chunks}
+        return {"filename": file.filename, "text": chunks }
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@pdfRouter.post("/makeQuestion")
+async def make_question(data: QuestionData):
+    try:
+        respuesta = handle_user_input(data.question)
+        print(respuesta)
+        return {"respuesta": respuesta}
     except Exception as e:
         print(e)
         raise HTTPException(status_code=500, detail=str(e))
